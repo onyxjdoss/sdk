f9d8360fa655abbe9d95e9ef7cb3dda9dfef19ef
jdoss <jdoss@onyxgs.com>
2015-04-15 15:32:52 -0400

    Bug

--- sample-website/public/theme/js/heat.js
+++ sample-website/public/theme/js/heat.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
 	$.getJSON('http://api.wunderground.com/api/d8ed4bf9d8f1b19b/conditions/q/GA/Brunswick.json', function(data) {
-		$('#output').html(data['current_observations']['heat_index_string']);
+		$('#output').html(data['current_observation']['heat_index_string']);
 	});
 })
