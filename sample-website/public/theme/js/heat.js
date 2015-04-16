c672e5483f5f9b036e49f60bf21c38122c1b0397
jdoss <jdoss@onyxgs.com>
2015-04-16 09:53:11 -0400

    Fixes to http(s) URLs.

--- sample-website/public/theme/js/heat.js
+++ sample-website/public/theme/js/heat.js
@@ -1,5 +1,8 @@
 $(document).ready(function() {
-	$.getJSON('http://api.wunderground.com/api/d8ed4bf9d8f1b19b/conditions/q/GA/Brunswick.json', function(data) {
-		$('#output').html(data['current_observation']['heat_index_string']);
+	$.getJSON('//api.wunderground.com/api/d8ed4bf9d8f1b19b/conditions/q/GA/Brunswick.json', function(data) {
+		$('#output').html(
+			data['current_observation']['heat_index_string'] + "<br />" +
+			data['current_observation']['wind_string']
+		);
 	});
 })
